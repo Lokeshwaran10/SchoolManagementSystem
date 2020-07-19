44f53bd16bca63352adce9ce00f9ed817f2d9c0a
Lokesh <officiallokesh96@gmail.com>
2020-07-19 13:29:15 +0530

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using SchoolManagement.Models;
+using System;
+
+[assembly: OwinStartupAttribute(typeof(SchoolManagement.Startup))]
+namespace SchoolManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        public void createRolesandUsers()
+        {
+          var context = new ApplicationDbContext();
+
+            var rolemanager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var usermanager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            
+
+            if(!rolemanager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                rolemanager.Create(role);
+
+               
+            }
+            if(!rolemanager.RoleExists("Teacher"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Teacher";
+                rolemanager.Create(role);
+
+            }
+            if (!rolemanager.RoleExists("Supervisor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Supervisor";
+                rolemanager.Create(role);
+
+            }
+        }
+    }
+}
